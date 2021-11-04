a0b3bb57ca8164b922b7f67d56f4f76c32e11198
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-04 07:55:39 +0700

    #16 - Add testIfLoggedUserHasAccessToRegularHomePage Test Case

--- src/test/java/com/hendisantika/SpringBootMultiLoginApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootMultiLoginApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
@@ -32,6 +34,15 @@ class SpringBootMultiLoginApplicationTests {
         resultActions
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("http://localhost/special/login"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testIfLoggedUserHasAccessToRegularHomePage() throws Exception {
+        final ResultActions resultActions = mockMvc.perform(get("/regular/home"));
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(view().name("regular/home"));
     }
 
 
