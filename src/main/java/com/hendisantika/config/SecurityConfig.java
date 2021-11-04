61b8e9967d7e029d52605e4e539925be1c599e99
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-04 07:46:00 +0700

    #4 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-login
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/4/2021
+ * Time: 7:45 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SecurityConfig {
+}
