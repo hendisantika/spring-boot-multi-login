af1b4cab372865107a205454f50fc3cabaf11b27
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-04 07:54:44 +0700

    #14 - Add testIfRegularHomePageIsSecured Test Case

--- src/test/java/com/hendisantika/SpringBootMultiLoginApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootMultiLoginApplicationTests.java
@@ -1,13 +1,29 @@
 package com.hendisantika;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class SpringBootMultiLoginApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void testIfRegularHomePageIsSecured() throws Exception {
+        final ResultActions resultActions = mockMvc.perform(get("/regular/home"));
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/regular/login"));
     }
 
 }
