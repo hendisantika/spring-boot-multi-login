201912c224217911862e9179d0f0dc29e62bd086
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-04 07:47:19 +0700

    #7 - Add configureGlobal

--- src/main/java/com/hendisantika/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.hendisantika.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -62,5 +64,13 @@ public class SecurityConfig {
                     .permitAll();
             //@formatter:on
         }
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER");
     }
 }
