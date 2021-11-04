f3ec8a0bbf18345a1e2468fbeb6a74b007bb9935
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-04 07:46:26 +0700

    #5 - Add SpecialSecurityConfig

--- src/main/java/com/hendisantika/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.hendisantika.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +17,27 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SecurityConfig {
+    @Configuration
+    @Order(1)
+    public static class SpecialSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            //@formatter:off
+            http
+                    .antMatcher("/special/**")
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/special/login")
+                    .defaultSuccessUrl("/special/home")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .logoutUrl("/special/logout")
+                    .permitAll();
+            //@formatter:on
+        }
+    }
 }
