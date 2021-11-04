3d7181b9625dbba0858fd31bba360425b0f2bec7
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-04 07:40:20 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/SpringBootMultiLoginApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultiLoginApplication.class, args);
+    }
+
+}
